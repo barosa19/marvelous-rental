965b8adfe695857354fc7aa6d3ed32e4b9a8a10b
Jaimon <jamin.hogan@gmail.com>
2023-03-07 19:52:19 -0500

    added version of displayRentalData

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var rentalArray;
 var zipTextElem = document.querySelector("#zipEntryText");
 var zipFormElem = document.querySelector("#zipForm");
+var rentalElem = document.querySelector(".houses");
 
 // realtor API globals
 var realtorSearchType = "rentalListings";
@@ -56,13 +57,53 @@ function saveRentalData() {
   
 }
 
-function getZip () {
-  return realtorZip;
-}
+// function getZip () {
+//   return realtorZip;
+// }
 
 //load rental data from rentalArray into side bar
 function displayRentalData() {
-  //load rental 
+  //go through rental object and create cards for each element in array
+  for (var i=0;i<rentalArray.length;i++){
+    //create card
+    var cardDiv = document.createElement("div");
+    cardDiv.setAttribute("class", "card");
+    //create card-section
+    var cardSection = document.createElement("div");
+    cardSection.setAttribute("class", "card-section");
+    cardSection.setAttribute("id", "address00");
+    var r = rentalArray[i]
+    var curAddress = `${r.addressLine1}<br>${r.city}, ${r.state} ${r.zipCode}`;
+    console.log("current address is:", curAddress);
+    cardSection.textContent = curAddress;
+    //create card-stats
+    var cardStats = document.createElement("div");
+    cardStats.setAttribute("class", "card-stats");
+    //create card-img
+    var cardImg = document.createElement("img");
+    cardImg.setAttribute("class", "card-img");
+    cardImg.setAttribute("src", "./assets/icons/rental01.png")
+    cardImg.setAttribute("alt", "rental property");
+    //create card-info
+    var cardInfo = document.createElement("div");
+    cardInfo.setAttribute("class", "card-info");
+    //create BR and BA
+    var bR = document.createElement("p");
+    bR.setAttribute("id", "br00");
+    var bA = document.createElement("p");
+    bA.setAttribute("id", "ba00");
+    //add br and ba to card-info
+    cardInfo.appendChild(bR, bA);
+    //add card-img and card-ingo to card-stats
+    cardStats.appendChild(cardImg, cardInfo);
+    //add card-stats to card-section
+    cardSection.appendChild(cardStats);
+    //add card-section to card
+    cardDiv.appendChild(cardSection);
+    //add card to houses class
+    rentalElem.appendChild(cardDiv);
+  }
+  
 }
 
 
@@ -79,6 +120,8 @@ zipFormElem.addEventListener('submit', function (event){
     saveRentalData();
     displayRentalData();
     console.log(rentalArray);
+    console.log("calling googlemapsstuff");
+    getGoogleAPI(realtorZip); //
   }
   else {
     console.log("not a number");
